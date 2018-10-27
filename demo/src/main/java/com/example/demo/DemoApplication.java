4a491121fdf35c8e3502b5b862c69dbf439924f8
Saek <Saek.kus.siit@gmail.com>
2018-10-27 18:28:26 +0700

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("aaaaa");
 		System.out.println("bbbbb");
+		System.out.println("ccccc");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
