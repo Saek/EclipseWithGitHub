18b3e586ced8db152bdadd1d4c5ca11e8fc6cfc9
Kraisaek <saek.kus.siit@gmail.com>
2018-10-29 10:52:57 +0700

    testbbb

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("aaaaa");
 		System.out.println("bbbbb");
+		System.out.println("ccccc");
+		System.out.println("dddd");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
